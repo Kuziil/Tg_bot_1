baa8b69cc04e2e208d6dd42735851d02336b371b
Daniil Kuznetsov <lfy050@yandex.ru>
2023-03-11 00:56:02 +0700

    Написал пояснений и аннотации.

--- bot.py
+++ bot.py
@@ -7,19 +7,25 @@ from keyboards.main_menu import set_main_menu
 
 
 async def main():
+    """
+    Функция конфигурирования и запуска бота
+    """
+    # инициализация бота и диспетчера
     config: Config = load_config()
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode='HTML')  # parse_mode='HTML' - поддержка HTML тегов
     dp: Dispatcher = Dispatcher()
 
+    # инициализируем меню
     await set_main_menu(bot)
 
+    # регистрация роутеров в диспетчере
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)  # пропуск накопившихся запросов
-    await dp.start_polling(bot)
+    await dp.start_polling(bot)  # запуск пулинга
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main())  # запуск функции main
