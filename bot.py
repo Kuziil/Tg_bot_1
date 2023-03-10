7c0b540f368197edd5e875409e253c3a6f12333a
Daniil Kuznetsov <lfy050@yandex.ru>
2023-03-10 22:24:59 +0700

    Прописываем функцию приема конфигураций и начала пулинга бота. Также указываем возможность приема HTML тегов в 10 строке; в 13 пропускаем накопившиеся запросы. На данном этапе бот работает, но не реагирует на запросы

---
+++ bot.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+
+async def main():
+    config: Config = load_config()
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML') # parse_mode='HTML' - поддержка HTML тегов
+    dp: Dispatcher = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)  # пропуск накопившихся запросов
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
