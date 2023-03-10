5a4dd59439c8fec6bdc78dbb570e131261d729f6
Daniil Kuznetsov <lfy050@yandex.ru>
2023-03-11 00:21:18 +0700

    Создал хэндлер реагирующий на /help.

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from lexicon.lexicon import LEXICON_COMMANDS
 
@@ -14,3 +14,13 @@ async def process_start_command(message: Message):
     :return: LEXICON_COMMANDS['/start']
     """
     await message.answer(text=LEXICON_COMMANDS['/start'])
+
+
+@router.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    """
+    Этот хэндлер срабатывает при отправке пользователем команды /help.
+    :param message:
+    :return: LEXICON['/help']
+    """
+    await message.answer(text=LEXICON_COMMANDS['/help'])
--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,7 +1,10 @@
+IN_THE_WORKS: str = ' еще в разработке'
+
 LEXICON: dict[str, str] = {
     'other_answer': 'Не понимаю, что вы имеете ввиду'
 }
 
 LEXICON_COMMANDS: dict[str, str] = {
-    '/start': 'Привет!'
+    '/start': '/start' + IN_THE_WORKS,
+    '/help': '/help' + IN_THE_WORKS
 }
