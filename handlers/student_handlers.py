46bfd7d400ba88f1574a07080b8414bff5f3fda7
Daniil Kuznetsov <lfy050@yandex.ru>
2023-03-11 20:17:55 +0700

    прописал хэндлер для заказа справок и проработал кнопку назад с использованием lambda.

--- handlers/student_handlers.py
+++ handlers/student_handlers.py
@@ -7,14 +7,28 @@ from lexicon.lexicon import LEXICON_FOR_STUDENTS
 student_router: Router = Router()
 
 
-@student_router.callback_query(Text(text='student'))
+@student_router.callback_query(
+    lambda x: True if x.data == 'back_after_certificates_for_students' or x.data == 'student' else False)
 async def process_student_press(callback: CallbackQuery):
     """
     Этот хэндлер срабатывает при нажатии кнопки 'Студент' и обрабатывает этот запрос.
-    :return: Возвращает новые кнопки заменяя текст над сообщением
+    :return: Возвращает новые кнопки заменяя текст над сообщением и новые кнопки
     """
     await callback.message.edit_text(
         text=LEXICON_FOR_STUDENTS['after_start_for_students'],
         reply_markup=main_students_kb('timetable', 'order_certificates', 'student card',
                                       'back_after_start_for_students')
     )
+
+
+@student_router.callback_query(Text(text='order_certificates'))
+async def process_order_certificates_push(callback: CallbackQuery):
+    """
+    Этот хэндлер срабатывает при нажатии кнопки 'Заказ справок' и обрабатывает этот запрос.
+    :return: Предлагает выбрать интересующие справки
+    """
+    print(callback, sep='\n')
+    await callback.message.edit_text(
+        text=LEXICON_FOR_STUDENTS['what_kind_of_certificates'],
+        reply_markup=main_students_kb('certificates_1', 'certificates_2', 'certificates_3',
+                                      'back_after_certificates_for_students'))
--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,5 +1,6 @@
 IN_THE_WORKS: str = ' еще в разработке'
 MENU_IN_THE_WORK: str = 'еще не переданы краткие пояснения'
+BACK: str = 'Назад'
 
 LEXICON: dict[str, str] = {
     'other_answer': 'Не понимаю, что вы имеете ввиду'
@@ -24,8 +25,13 @@ LEXICON_FOR_STUDENTS: dict[str, str] = {
     'after_start_for_students': 'Выберите интересующий вас раздел:',
     'timetable': 'Расписание',
     'order_certificates': 'Заказ справок',
+    'what_kind_of_certificates': 'Какую справку требуется заказать?',
+    'certificates_1': 'С места учебы',
+    'certificates_2': 'Копия диплома',
+    'certificates_3': 'Оригинал диплома',
+    'back_after_certificates_for_students': BACK + '1',
     'student card': 'Студенческий билет',
-    'back_after_start_for_students': 'Назад'
+    'back_after_start_for_students': BACK
 }
 
 LEXICON_FOR_USERS: dict[str, str] = {
